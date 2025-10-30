4b010f828fc4717f00e54c7e86fd87a4cde62d6d
Gabriel Gonzalez <gagonzalez@symbotic.com>
2025-10-30 09:41:47 -0500

    Add Snowflake connection credentials template

---
+++ credentials.py
@@ -0,0 +1,7 @@
+# -- Snowflake connection creds 
+USER = "<none selected>"
+PASSWORD = "<none selected>"
+ACCOUNT = "<none selected>"
+WAREHOUSE = "<none selected>"
+DATABASE = "<none selected>"
+SCHEMA = "<none selected>"
